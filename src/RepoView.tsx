6f0e6fe5ad3ae736b40ca58b7a4d672fb45277f0
Anton Strogonoff <anton@strogonoff.name>
2020-11-20 14:53:21 +0900

    feat: expand detail view

--- src/RepoView.tsx
+++ src/RepoView.tsx
@@ -8,24 +8,32 @@ Object.assign(console, log);
 import { RegistryView } from '@riboseinc/paneron-registry-kit/views';
 import { PropertyDetailView } from '@riboseinc/paneron-registry-kit/views/util';
 import { ItemClassConfiguration } from '@riboseinc/paneron-registry-kit/types/views';
-import { ControlGroup, InputGroup, Tag, TextArea } from '@blueprintjs/core';
+import { ControlGroup, InputGroup, NonIdealState, Tag, TagInput, TextArea } from '@blueprintjs/core';
 
 
 const DoubleLanguagePropertyDetailView: React.FC<{
-  title: string
-  values: { [langID: string]: string }
-  ValueWidget: React.FC<{ value: string }>
-}> = function ({ title, values, ValueWidget }) {
-  return <PropertyDetailView title={title}>
+  values: { [langID: string]: any }
+  ValueWidget: React.FC<{ value: any }>
+}> = function ({ values, ValueWidget }) {
+  return (
     <ControlGroup fill>
       {Object.entries(values).map(([langID, value]) =>
-        <div>
+        <div css={css`width: 50%;`}>
           <Tag minimal css={css`margin-bottom: .25rem;`}>{langID}</Tag>
           <ValueWidget value={value} />
         </div>
       )}
     </ControlGroup>
-  </PropertyDetailView>
+  )
+}
+
+
+const MaybeTextarea: React.FC<{ value: any }> = function ({ value }) {
+  if (value || (value as string[]).length > 0) {
+    return <TextArea css={css`width: 100%; height: 100%;`} readOnly value={value} />;
+  } else {
+    return <span css={css`padding: 0 1rem;`}>—</span>;
+  }
 }
 
 
@@ -143,24 +151,88 @@ const code: ItemClassConfiguration<SymbolData> = {
         identifier,
         title,
         description,
+        authors,
+        keywords,
+        replacing,
+        relevantTCs,
+        fieldOfApplication,
+        relevantPublications,
+        productArea,
+        notes,
+        remarks,
       } = itemData;
-      return (
-        <div className={className}>
-          <PropertyDetailView title="Identifier">
-            <p>{identifier}</p>
-          </PropertyDetailView>
-          <DoubleLanguagePropertyDetailView
-            title="Title"
-            values={title}
-            ValueWidget={({ value }) => <InputGroup readOnly value={value} />}
-          />
-          <DoubleLanguagePropertyDetailView
-            title="Description"
-            values={description}
-            ValueWidget={({ value }) => <TextArea css={css`width: 100%; height: 100%;`} readOnly value={value} />}
-          />
-        </div>
-      );
+      if (identifier && title && description && keywords) {
+        return (
+          <div className={className}>
+            <PropertyDetailView title="Identifier">
+              {identifier}
+            </PropertyDetailView>
+
+            <PropertyDetailView title="Title">
+              <DoubleLanguagePropertyDetailView
+                values={title}
+                ValueWidget={({ value }) => <InputGroup readOnly value={value} />}
+              />
+            </PropertyDetailView>
+
+            <PropertyDetailView title="Product area">
+              <DoubleLanguagePropertyDetailView
+                values={productArea}
+                ValueWidget={MaybeTextarea}
+              />
+            </PropertyDetailView>
+
+            <PropertyDetailView title="Description">
+              <DoubleLanguagePropertyDetailView
+                values={description}
+                ValueWidget={MaybeTextarea}
+              />
+            </PropertyDetailView>
+
+            <PropertyDetailView title="Notes">
+              <DoubleLanguagePropertyDetailView
+                values={notes}
+                ValueWidget={MaybeTextarea}
+              />
+            </PropertyDetailView>
+
+            <PropertyDetailView title="Remarks">
+              <DoubleLanguagePropertyDetailView
+                values={remarks}
+                ValueWidget={MaybeTextarea}
+              />
+            </PropertyDetailView>
+
+            <PropertyDetailView title="Authors">
+              <TagInput disabled leftIcon="user" values={authors} />
+            </PropertyDetailView>
+            <PropertyDetailView title="Function">
+              <TagInput disabled leftIcon="locate" values={itemData.function} />
+            </PropertyDetailView>
+            <PropertyDetailView title="Field of application">
+              <TagInput disabled leftIcon="search-around" values={fieldOfApplication} />
+            </PropertyDetailView>
+            <PropertyDetailView title="Replacing">
+              <TagInput disabled leftIcon="flows" values={replacing} />
+            </PropertyDetailView>
+            <PropertyDetailView title="Relevant TCs">
+              <TagInput disabled leftIcon="people" values={relevantTCs} />
+            </PropertyDetailView>
+            <PropertyDetailView title="Relevant publications">
+              <TagInput disabled leftIcon="book" values={relevantPublications} />
+            </PropertyDetailView>
+
+            <PropertyDetailView title="Keywords">
+              <DoubleLanguagePropertyDetailView
+                values={keywords}
+                ValueWidget={({ value }) => <TagInput disabled leftIcon="tag" values={value} />}
+              />
+            </PropertyDetailView>
+          </div>
+        );
+      } else {
+        return <NonIdealState icon="heart-broken" title="Error displaying item" />;
+      }
     },
     editView: () => <span>TBD</span>,
   },
