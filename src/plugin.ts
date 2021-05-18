ab92db3f1b602e0115ad1c974464682ffd24f348
Anton Strogonoff <anton@strogonoff.name>
2021-05-18 09:29:26 +0300

    chore: fix extension name

--- src/plugin.ts
+++ src/plugin.ts
@@ -2,7 +2,7 @@ import { makeExtension } from '@riboseinc/paneron-extension-kit';
 
 export default makeExtension({
   mainView: () => import('./RepoView'),
-  name: "ICS Codes Registry",
+  name: "IEC 60417 Symbol Registry",
   requiredHostAppVersion: "^1.0.0-beta1",
   datasetMigrations: {},
   datasetInitializer: () => import('./migrations/initial'),
